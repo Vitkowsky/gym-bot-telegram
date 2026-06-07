1c8283e499ff1369f759e8752aab6edf65342682
David <89921515+Vitkowsky@users.noreply.github.com>
2026-06-07 16:20:44 +0300

    Update workout_bot_0.1.py
    Edition code

--- workout_bot_0.1.py
+++ workout_bot_0.1.py
@@ -11,8 +11,8 @@ from telegram.ext import (
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-import os
-TOKEN = os.environ.get("TOKEN")
+
+TOKEN = "8988267445:AAGdwuJL9ilgKFY7--ou9nuntcDR0dFvUFo"
 
 # ─────────── Состояния ───────────
 (
